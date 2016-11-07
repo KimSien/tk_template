9bad4da02feb5b9b52869ca882ceb0b067eb4ecc
toysking <tksok2009@gmail.com>
2016-11-07 23:02:50 +0900

    modify not move foundation slide add motion-ui

--- public/00config_css.php
+++ public/00config_css.php
@@ -1,5 +1,7 @@
 <!-- Compressed CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.css">
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.2.2/motion-ui.min.css">"
+
 <link rel="stylesheet" href="<?php TEMPLATEPATH; ?>/css/mystyle.css">
 
